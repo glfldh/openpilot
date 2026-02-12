7241857ebb90ebd6ee5b452bcdcdef600488fa40
elkoled <elkoled@gmail.com>
2026-02-11 16:44:42 -0800

    poweroff

--- system/hardware/tici/hardware.py
+++ system/hardware/tici/hardware.py
@@ -322,11 +322,9 @@ class Tici(HardwareBase):
 
   def shutdown(self):
     if self.get_device_type() == "mici":
-      self.set_ship_mode()  # starts 13s timer, then PMIC cuts VPH_PWR
+      self.set_ship_mode()  # arms BATFET reversal, engaged on PS_HOLD release
       gpio_set(GPIO.SOM_ST_IO, 0)  # stop fan
-      os.system("sudo halt")  # clean shutdown without PS_HOLD release
-    else:
-      os.system("sudo poweroff")
+    os.system("sudo poweroff")
 
   def get_thermal_config(self):
     intake, exhaust, case = None, None, None
