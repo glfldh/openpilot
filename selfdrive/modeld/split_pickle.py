63e231b86cd0aa99cef40612bfd7e1edc1724023
YassineYousfi <yyousfi1@binghamton.edu>
2026-02-11 19:45:39 -0800

    better

--- selfdrive/modeld/split_pickle.py
+++ selfdrive/modeld/split_pickle.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
-"""Split a pickle file produced by compile3.py into external parts, then remove the original."""
+"""Create a pickle pointer file with external data parts. Usage: split_pickle.py <input> <output>"""
 import sys
 from openpilot.selfdrive.modeld.external_pickle import dump_external_pickle, load_external_pickle
 
-path = sys.argv[1]
-obj = load_external_pickle(path)
-dump_external_pickle(obj, path)
+input_path = sys.argv[1]
+output_path = sys.argv[2] if len(sys.argv) > 2 else input_path
+obj = load_external_pickle(input_path)
+dump_external_pickle(obj, output_path)
