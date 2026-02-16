7ae25d19039c2d8bbb9061b4623e3dc4380b0598
elkoled <elkoled@gmail.com>
2026-02-16 08:00:40 -0800

    debug

--- system/hardware/tici/ship-mode.sh
+++ system/hardware/tici/ship-mode.sh
@@ -1,4 +1,7 @@
   #!/bin/sh
+  echo "ship-mode: called with $1" > /dev/kmsg
   [ "$1" = "poweroff" ] && grep -q "mici" /sys/firmware/devicetree/base/model && \
     chmod a+w /sys/class/power_supply/battery/set_ship_mode && \
-    echo 1 > /sys/class/power_supply/battery/set_ship_mode
+    echo "ship-mode: chmod ok" > /dev/kmsg && \
+    echo 1 > /sys/class/power_supply/battery/set_ship_mode && \
+    echo "ship-mode: write ok" > /dev/kmsg
