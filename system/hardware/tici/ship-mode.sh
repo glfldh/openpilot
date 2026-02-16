2d5827598fe1a52552b3f8586f75be556e84157e
elkoled <elkoled@gmail.com>
2026-02-16 07:53:34 -0800

    permission

--- system/hardware/tici/ship-mode.sh
+++ system/hardware/tici/ship-mode.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
-[ "$1" = "poweroff" ] && grep -q "mici" /sys/firmware/devicetree/base/model && \
-  echo 1 > /sys/class/power_supply/battery/set_ship_mode
+  #!/bin/sh
+  [ "$1" = "poweroff" ] && grep -q "mici" /sys/firmware/devicetree/base/model && \
+    chmod a+w /sys/class/power_supply/battery/set_ship_mode && \
+    echo 1 > /sys/class/power_supply/battery/set_ship_mode
